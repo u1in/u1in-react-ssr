3b8f9f3f443970b11f31cfbec8700b401833ca03
U1in <714896166@qq.com>
2024-03-03 16:57:10 +0800

    Add Page1

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,16 @@
 import Home from "@src/pages/home";
+import Page1 from "@src/pages/page1";
 
 const routes = [
   {
     key: "/home",
     path: "/home",
     component: Home,
+  },
+  {
+    key: "/page1",
+    path: "/page1",
+    component: Page1,
   },
 ];
 
