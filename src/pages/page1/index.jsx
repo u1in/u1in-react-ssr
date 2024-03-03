e09d4c34511ef8980436ec751e9a5ef6ed24a521
U1in <714896166@qq.com>
2024-03-03 16:57:19 +0800

    Add Page1

--- src/pages/page1/index.jsx
+++ src/pages/page1/index.jsx
@@ -1,31 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./index.less";
 import HomeSVG from "@images/home.svg";
 
 const Page = (props) => {
-  const navigate = useNavigate();
   return (
-    <div className="home-container">
+    <div className="page-container">
       <HomeSVG height="200" width="300" />
-      <div className="title">u1in-react-isomorphic-render</div>
-      <div className="library">
-        <span>React-Isomorphic-Render</span>
-        <span>Webpack 5</span>
-        <span>React 18</span>
-        <span>React Router 6</span>
-        <span>Babel 7</span>
-      </div>
+      <div className="title">Page1</div>
       <pre>PreLoad Data: {JSON.stringify(props?.INITDATA)}</pre>
-      <div
-        className="link"
-        onClick={() => {
-          navigate("/page1");
-        }}
-      >
-        Test Navigate To Page1
-      </div>
     </div>
   );
 };
